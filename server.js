5de73a75e2932eec67355b7564eab206e9aedeb2
Taegan Loyzelle <tloyzelle@gmail.com>
2021-09-02 21:16:59 -0400

    deploy updates

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Budget',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Budget',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
