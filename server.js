bf9db8a6741b4aa1403d4f8ddb42a5c60bfbfacc
Taegan Loyzelle <tloyzelle@gmail.com>
2021-09-02 21:09:18 -0400

    adding updated in mongoose connect for deployment to heroku

--- server.js
+++ server.js
@@ -15,12 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/Budget", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/Budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
